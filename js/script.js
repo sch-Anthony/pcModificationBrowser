a89d972a8ee84eb334c26d40964aa0a95b98a319
sch-Anthony <songchihun0219@gmail.com>
2026-03-11 22:53:10 +0900

    3d model mech added

--- js/script.js
+++ js/script.js
@@ -1,3 +1,51 @@
+import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.160.0/build/three.module.js";
+import { OrbitControls } from "https://cdn.jsdelivr.net/npm/three@0.160.0/examples/jsm/controls/OrbitControls.js";
+import { GLTFLoader } from "https://cdn.jsdelivr.net/npm/three@0.160.0/examples/jsm/loaders/GLTFLoader.js";
+
+const viewContainer = document.getElementById("viewerArea");
+const modelScene = new THREE.scene;
+const camera = new THREE.PerspectiveCamera(
+  60,
+  viewContainer.clientHeight / viewContainer.clientWidth,
+  1000,
+  0.1
+);
+camera.position.set(0,1,4);
+const renderer = new THREE.WebGLRenderer({antialias:true, alpha:true});
+renderer.setSize(viewContainer.clientHeight,viewContainer.clientWidth);
+
+viewContainer.appendChild(renderer.domElement)
+
+const light1 = new THREE.HemisphereLight(0xffffff,0x444444,1);
+scene.add(light1);
+
+const light2 = new THREE.DirectionalLight(0xffffff,1);
+light2.position.set(5,5,5);
+scene.add(light2);
+
+const loader = new GLTFLoader();
+
+let model;
+
+function loadModel(path){
+if(model){
+scene.remove(model);
+}
+loader.load(path,(gltf)=>{
+model = gltf.scene;
+scene.add(model);
+});
+}
+
+loadModel("./models/CPU.glb")
+
+function animate(){
+requestAnimationFrame(animate);
+controls.update();
+renderer.render(scene,camera);
+}
+animate();
+
 const models = [
 {
 name:"A",
@@ -20,6 +68,8 @@ document.getElementById("infoTitle").innerText =
 models[currentModel].name;
 document.getElementById("modelDescription").innerText =
 models[currentModel].description;
+
+loadModel("./models/" + models[currentModel].name + ".glb");
 
 }
 
