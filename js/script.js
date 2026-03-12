2e6bc44b1d80b499ad8f50b4e119e107bef1bf37
sch-Anthony <songchihun0219@gmail.com>
2026-03-12 20:08:09 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.160.0/build/three.module.js";
+import * as THREE from
+"https://cdn.jsdelivr.net/npm/three@0.160.0/build/three.module.js";
 
 import { OrbitControls } from
 "https://cdn.jsdelivr.net/npm/three@0.160.0/examples/jsm/controls/OrbitControls.js";
